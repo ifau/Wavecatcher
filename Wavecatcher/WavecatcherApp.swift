7ad9bb7ebe0ce9f2f226df18c64e98c72d5bb88b
Evgeny Seliverstov <ifau@me.com>
2023-11-01 12:36:16 +0800

    Initial commit

---
+++ Wavecatcher/WavecatcherApp.swift
@@ -0,0 +1,15 @@
+//
+//  WavecatcherApp.swift
+//  Wavecatcher
+//
+
+import SwiftUI
+
+@main
+struct WavecatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
